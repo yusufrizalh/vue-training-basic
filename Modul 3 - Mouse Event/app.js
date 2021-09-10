b4a3b4654ca9f1b1bbb18bd07c54a364146bb305
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-10 11:13:32 +0700

    update tree

--- Modul 3 - Mouse Event/app.js
+++ Modul 3 - Mouse Event/app.js
@@ -5,18 +5,24 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBooks() {
-        this.showBooks = !this.showBooks
+      this.showBooks = !this.showBooks;
     },
     handleEvent(event, data) {
-        console.log(event, event.type)
-        if (data) {
-            console.log(data)
-        }
-    }
+      console.log(event, event.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(event) {
+        this.x = event.offsetX
+        this.y = event.offsetY
+    },
   },
 });
 
---
+++ Modul 3 - Outputting Lists/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(event, data) {
+      console.log(event, event.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+  },
+});
+
+app.mount("#app");
