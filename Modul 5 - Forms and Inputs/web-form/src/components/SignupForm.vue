42ef336e50411d5b594d03266dcead650cd3877a
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-12 11:37:04 +0700

    update tree

---
+++ Modul 5 - Forms and Inputs/web-form/src/components/SignupForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email" />
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+      <option value="tester">Tester</option>
+      <option value="dbadmin">Database Admin</option>
+    </select>
+    <div class="terms">
+      <input type="checkbox" required v-model="terms">
+      <label>Accept terms and conditions</label>
+    </div>
+    <div>
+      <input type="checkbox" value="inixindo" v-model="names">
+      <label>Inixindo</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yusuf" v-model="names">
+      <label>Yusuf</label>
+    </div>
+    <div>
+      <input type="checkbox" value="rizal" v-model="names">
+      <label>Rizal</label>
+    </div>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <p>Names: {{ names }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: 'yusuf@email.com',
+      password: '',
+      role: 'designer',
+      terms: false,
+      names: []
+    }
+  }
+}
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaaaaa;
+  display: inline-block;
+  margin: 25px 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input, select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #dddddd;
+  color: #555555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+</style>
